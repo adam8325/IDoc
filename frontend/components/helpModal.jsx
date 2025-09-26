5a1690d3f2572328a46161dd6202ca7657501286
Adam Eleraky <adam_eleraky@hotmail.com>
2025-09-26 07:56:44 +0200

    Responsive UI added for modal

--- frontend/components/helpModal.jsx
+++ frontend/components/helpModal.jsx
@@ -21,43 +21,49 @@ export default function HelpModal() {
             <HelpButton onClick={handleOpen}/>
             {openModal && (
                 <div className="fixed inset-0 flex items-center justify-center bg-black/80">
-                    <div className="flex flex-col items-center gap-4 bg-white rounded-lg py-4 px-6 w-2/5 h-4/5 mx-4 border">
-                        <button onClick={handleClose} className="ml-auto font-bold text-gray-500 rounded cursor-pointer">
-                                <X className="h-5 w-5"/>
-                        </button>
-                        <h2 className="text-3xl font-bold text-center">Sådan virker IDoc</h2>
+                    <div className="flex flex-col items-center gap-2 sm:gap-8 bg-white rounded-lg py-2 px-2 sm:py-6 sm:px-6 w-3/5 sm:w-2/5 h-4/5 mx-4 border">
+                        <div className="grid grid-cols-[1fr_4fr_1fr] items-start w-full">
+                            <div></div> 
+                            <h2 className="text-md sm:text-3xl font-bold text-center">Sådan virker IDoc</h2>
+                            <button 
+                                onClick={handleClose} 
+                                className="justify-self-end font-bold text-gray-500 rounded cursor-pointer"
+                            >
+                                <X className="h-3 w-3 sm:h-5 sm:w-5"/>
+                            </button>
+                        </div>
 
-                        <div className="flex flex-col items-center justify-center gap-12 text-center h-full w-full">
-                            <div className="flex items-start justify-center gap-2">
-                                <div className="flex flex-col gap-3 w-1/2 order-2">
-                                    <h3 className="text-xl font-bold">Indsæt din kode</h3>
-                                    <p> Indsæt din kode direkte i tekstfeltet eller upload en fil med din kode.</p>
+                        <div className="flex flex-col items-center justify-center gap-4 sm:gap-12 text-center h-full w-full sm:mt-4">
+                            <div className="flex flex-col sm:flex-row items-center sm:items-start justify-center gap-2">
+                                <div className="flex flex-col gap-1 sm:gap-3 w-3/4 sm:w-1/2 order-1 sm:order-2">
+                                    <h3 className="text-sm sm:text-xl font-bold">Indsæt din kode</h3>
+                                    <p className="text-[9px] sm:text-sm"> Indsæt din kode direkte i tekstfeltet eller upload en fil med din kode.</p>
                                 </div>
-                                <div className="w-1/2 order-1 flex justify-center">
+                                <div className="w-3/4 sm:w-1/2 order-2 sm:order-1 flex justify-center">
                                     <Image  src="/images/upload.png" alt="Upload Image" width={250} height={200} className="drop-shadow-[0_0_4px_rgba(0,0,0,0.3)]"/>
                                 </div>                                
                             </div>
                             
-                            <div className="flex items-start justify-center gap-2">
-                                <div className="flex flex-col gap-3 w-1/2">
-                                    <h3 className="text-xl font-bold">Vælg typen</h3>
-                                    <p>Vælg mellem en teknisk dokumentation til udviklere eller                               brugervenlige guides til alle.</p>
+                            <div className="flex flex-col sm:flex-row items-center sm:items-start justify-center gap-2">
+                                <div className="flex flex-col gap-1 sm:gap-3 w-3/4 sm:w-1/2">
+                                    <h3 className="text-sm sm:text-xl font-bold">Vælg typen</h3>
+                                    <p className="text-[9px] sm:text-sm">Vælg mellem en teknisk dokumentation til udviklere eller brugervenlige output til alle.</p>
                                 </div>
-                                <div className="w-1/2 order-1 flex justify-center">
+                                <div className="w-3/4 sm:w-1/2 order-1 flex justify-center">
                                     <Image src="/images/type.png" alt="Type Image" width={250} height={160} className="drop-shadow-[0_0_4px_rgba(0,0,0,0.3)]"/>
                                 </div>
                                 
                             </div>                            
                             
-                            <div className="flex items-start justify-center gap-2">
-                                <div className="flex flex-col gap-3 w-1/2 order-2">
-                                    <h3 className="text-xl font-bold">Upload Kb-fil</h3>
-                                    <p>
+                            <div className="flex flex-col sm:flex-row items-center sm:items-start justify-center gap-2">
+                                <div className="flex flex-col gap-1 sm:gap-3 w-3/4 sm:w-1/2 order-1 sm:order-2">
+                                    <h3 className="text-sm sm:text-xl font-bold">Upload Kb-fil</h3>
+                                    <p className="text-[9px] sm:text-sm">
                                         Vælger du at uploade en fil med jeres specifikke kodestandarder, så
                                         vil programmet tilrette din kode ud fra jeres retningslinjer.
                                     </p>
                                 </div>
-                                <div className="w-1/2 order-1 flex justify-center">
+                                <div className="w-3/4 sm:w-1/2 order-2 sm:order-1 flex justify-center">
                                     <Image src="/images/context.png" alt="Context Image" width={250} height={160} className="drop-shadow-[0_0_4px_rgba(0,0,0,0.3)]"/>
                                 </div>                                
                             </div>                           
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -20,11 +20,15 @@ export default function Home() {
       <div className="min-h-screen w-3/4 flex flex-col mx-auto border border-stone-100 rounded-sm ">
         <Header/>
         <main className='py-4 px-6 flex flex-col gap-4 bg-[linear-gradient(135deg,hsl(250_50%_96%),hsl(280_50%_98%))]'>
-          <div className="flex items-start justify-between py-4">
-            <div className="flex-1 flex justify-center">
-              <Intro />
-            </div>
-            <HelpModal /> 
+          <div className="flex flex-col items-center gap-8 sm:grid sm:grid-cols-[1fr_2fr_1fr] w-full sm:items-start sm:justify-center py-4">
+            <div className='hidden sm:block' ></div>
+            <div>
+              <Intro/>      
+            </div>              
+            <div className="sm:justify-self-end justify-center">
+              <HelpModal /> 
+            </div>    
+            
           </div>
           <Upload text={text}
                   setText={setText}
